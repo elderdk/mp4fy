48ee05d136b14f2520208d501d6e810bd4557eae
daniel <elderdk@DESKTOP-10UCPFB.localdomain>
2023-01-02 13:21:46 +0900

    fix

--- tests/test_handler.py
+++ tests/test_handler.py
@@ -7,7 +7,7 @@ class TestHandlerCase(unittest.TestCase):
     def test_download(self):
         url = "https://www.youtube.com/watch?v=z5JKXbhtSY8"
         filename, info = download(url)
-        self.assertEqual(info['title'], "Over The Garden Wall Official Soundtrack | Money for School – The Blasting Company | WaterTower")
+        self.assertEqual(info['title'], "Over The Garden Wall Official Soundtrack | Adelaide Parade – The Blasting Company | WaterTower")
 
 if __name__ == '__main__':
     unittest.main()
