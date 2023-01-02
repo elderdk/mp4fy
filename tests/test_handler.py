6cca89245e23d8bc1ead9ad9950f340c80c4cc66
daniel <elderdk@DESKTOP-10UCPFB.localdomain>
2023-01-02 13:18:42 +0900

    fix test url

--- tests/test_handler.py
+++ tests/test_handler.py
@@ -5,7 +5,7 @@ from downloader import download
 class TestHandlerCase(unittest.TestCase):
 
     def test_download(self):
-        url = "https://www.youtube.com/watch?v=z5JKXbhtSY8&list=PLKzpt3zYOKYqQXc2n3xRR9eDmTohMh6xZ&index=6&ab_channel=WaterTowerMusic"
+        url = "https://www.youtube.com/watch?v=z5JKXbhtSY8"
         filename, info = download(url)
         self.assertEqual(info['title'], "Over The Garden Wall Official Soundtrack | Money for School – The Blasting Company | WaterTower")
 
