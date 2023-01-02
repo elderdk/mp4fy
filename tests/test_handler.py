c9184756faf367c11a8765d452caecbb15b8e31a
daniel <elderdk@DESKTOP-10UCPFB.localdomain>
2023-01-02 13:13:51 +0900

    add test

--- tests/test_handler.py
+++ tests/test_handler.py
@@ -1,11 +1,13 @@
 import unittest
-import main
+from downloader import download
 
 
 class TestHandlerCase(unittest.TestCase):
 
-    def test_response(self):
-        pass
+    def test_download(self):
+        url = "https://www.youtube.com/watch?v=z5JKXbhtSY8&list=PLKzpt3zYOKYqQXc2n3xRR9eDmTohMh6xZ&index=6&ab_channel=WaterTowerMusic"
+        filename, info = download(url)
+        self.assertEqual(info['title'], "Over The Garden Wall Official Soundtrack | Money for School – The Blasting Company | WaterTower")
 
 if __name__ == '__main__':
     unittest.main()
