f7adb80e6d2f108503a5cd4d9e00789629f73e07
daniel <elderdk@DESKTOP-10UCPFB.localdomain>
2023-02-28 00:56:43 +0900

    update lambda ytdlp layer version to 3

--- main.py
+++ main.py
@@ -27,7 +27,7 @@ def handler(event, context):
 if __name__ == "__main__":
     url = "https://youtu.be/6O3MzPeomqs"
     long_video_url = (
-        "https://www.youtube.com/watch?v=L_LUpnjgPso&ab_channel=Fireplace10hours"
+        "https://www.youtube.com/watch?v=hG3lNBp7Xw4&ab_channel=katsuramy%21%21"
     )
     result = handler({"queryStringParameters": {"url": long_video_url}}, {})
     print(result)
